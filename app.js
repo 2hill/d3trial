bcce3bdbb2e3477992d895a5b32df2076b968a69
2hill <jamil.rahou@laposte.net>
2017-09-21 18:40:12 +0200

    add svg :fire:

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 "use strict";
 
-var data = [ 5, 8, 15, 19, 32, 42];
 
-var x = d3.scale.linear().domain([0, d3.max(data)]).range([0, 420]);
+    let data = [4, 8, 15, 16, 23, 42];
 
-d3.select(".chart").selectAll("div").data(data).enter().append("div").style("width", function(d) {
-  return x(d) + "px";
-}).text(function(d) {
-  return d;
-});
+    let width = 420,
+        barHeight = 20;
+
+    let x = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([0, width]);
+
+    let chart = d3.select(".chart")
+        .attr("width", width)
+        .attr("height", barHeight * data.length);
+
+    let bar = chart.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+    bar.append("rect")
+        .attr("width", x)
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", function(d) { return x(d) - 3; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return d; });
